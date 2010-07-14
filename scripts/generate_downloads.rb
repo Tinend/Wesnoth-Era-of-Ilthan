4652178203b2e1455a6d41d275a9dc8e2b7a726f
Bernhard F. Brodowsky <brodowsb@student.ethz.ch>
2010-07-14 22:39:56 +0200

    Download script verbessert.

--- scripts/generate_downloads.rb
+++ scripts/generate_downloads.rb
@@ -4,12 +4,12 @@ $:.unshift( File.dirname( __FILE__ ) )
 
 require 'enumeration_utils'
 
-DOWNLOAD_FILE = File.join( "downloads", "Era_of_Ilthan.tar" )
+DOWNLOAD_FILE = File.join( "Era_of_Ilthan", "downloads", "Era_of_Ilthan.tar" )
 TAR_COMMAND = "tar -cf " + DOWNLOAD_FILE + " "
 COMPRESS_COMMAND = "gzip " + DOWNLOAD_FILE
 RM_COMMAND = "rm " + DOWNLOAD_FILE + " " + DOWNLOAD_FILE + ".gz"
 
-Dir.chdir( File.join( File.dirname( __FILE__ ), ".." ) )
+Dir.chdir( File.join( File.dirname( __FILE__ ), "..", ".." ) )
 
 class DownloadCollector < FileEnumerator
   def acceptable_file_name?( file_name )
@@ -20,7 +20,7 @@ end
 dc = DownloadCollector.new
 
 file_list = []
-dc.each_file( '.' ) { |f| file_list.push( f ) }
+dc.each_file( "Era_of_Ilthan" ) { |f| file_list.push( f ) }
 system( RM_COMMAND )
 system( TAR_COMMAND + file_list.join( " " ) )
 system( COMPRESS_COMMAND )
