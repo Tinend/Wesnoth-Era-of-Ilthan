c7dabce565ef269c6c7d5228b9b49e6500aa881e
Bernhard F. Brodowsky <brodowsb@student.ethz.ch>
2010-07-17 00:45:29 +0200

    Downloadgenerator verbessert

--- scripts/generate_downloads.rb
+++ scripts/generate_downloads.rb
@@ -4,12 +4,14 @@ $:.unshift( File.dirname( __FILE__ ) )
 
 require 'enumeration_utils'
 
-DOWNLOAD_FILE = File.join( "Era_of_Ilthan", "downloads", "Era_of_Ilthan.tar" )
+Dir.chdir( File.join( File.dirname( __FILE__ ), "..", ".." ) )
+
+VERSION_FILE = File.join( "Era_of_Ilthan", "downloads", "version" )
+version = 1 + File.read( VERSION_FILE ).to_i
+File.open( VERSION_FILE, 'w' ) { |f| f.puts version }
+DOWNLOAD_FILE = File.join( "Era_of_Ilthan", "downloads", "Era_of_Ilthan_0.0.#{version}.tar" )
 TAR_COMMAND = "tar -cf " + DOWNLOAD_FILE + " "
 COMPRESS_COMMAND = "gzip " + DOWNLOAD_FILE
-RM_COMMAND = "rm " + DOWNLOAD_FILE + " " + DOWNLOAD_FILE + ".gz"
-
-Dir.chdir( File.join( File.dirname( __FILE__ ), "..", ".." ) )
 
 class DownloadCollector < FileEnumerator
   def accept_file_name?( file_name )
@@ -21,6 +23,5 @@ dc = DownloadCollector.new
 
 file_list = []
 dc.each_file( "Era_of_Ilthan" ) { |f| file_list.push( f ) }
-system( RM_COMMAND )
 system( TAR_COMMAND + file_list.join( " " ) )
 system( COMPRESS_COMMAND )
