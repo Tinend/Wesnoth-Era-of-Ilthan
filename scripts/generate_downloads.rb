44741427ab81b81e18b493edd7c4b99769703fa8
Bernhard F. Brodowsky <brodowsb@student.ethz.ch>
2010-07-17 11:52:10 +0200

    Paar Attacken & Rasse vom dark sorcerer korrigiert

--- scripts/generate_downloads.rb
+++ scripts/generate_downloads.rb
@@ -9,7 +9,7 @@ Dir.chdir( File.join( File.dirname( __FILE__ ), "..", ".." ) )
 VERSION_FILE = File.join( "Era_of_Ilthan", "downloads", "version" )
 version = 1 + File.read( VERSION_FILE ).to_i
 File.open( VERSION_FILE, 'w' ) { |f| f.puts version }
-DOWNLOAD_FILE = File.join( "Era_of_Ilthan", "downloads", "Era_of_Ilthan_0.0.#{version}.tar" )
+DOWNLOAD_FILE = File.join( "Era_of_Ilthan", "downloads", "Era_of_Ilthan-0.0.#{version}.tar" )
 TAR_COMMAND = "tar -cf " + DOWNLOAD_FILE + " "
 COMPRESS_COMMAND = "gzip " + DOWNLOAD_FILE
 
